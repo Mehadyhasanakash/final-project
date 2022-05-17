72dc664e47a084fa5551bc7cfc14cb0fd3ff89df
Mehadyhasanakash <mehadyhasan1000@gmail.com>
2022-05-17 17:18:25 +0600

    pages-create

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
+import Nav from "./Componets/Nav";
 
 function App() {
   return (
     <div >
-     <div class="card card-compact w-96 bg-base-100 shadow-xl">
-  <figure><img src="https://api.lorem.space/image/shoes?w=400&h=225" alt="Shoes" /></figure>
-  <div class="card-body">
-    <h2 class="card-title">Shoes!</h2>
-    <p>If a dog chews shoes whose shoes does he choose?</p>
-    <div class="card-actions justify-end">
-      <button class="btn btn-primary">Buy Now</button>
-    </div>
-  </div>
-</div>
+     <Nav/>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,30 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {},
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+        
+"primary": "#176F68",
+        
+"secondary": "#ffC000",
+        
+"accent": "#FFED05",
+        
+"neutral": "#F3F4F6",
+        
+"base-100": "#FFFFFF",
+        
+"info": "#98A8DD",
+        
+"success": "#1BBB70",
+        
+"warning": "#DF7E07",
+        
+"error": "#FA5C5C",
+        },
+      },
+    ],
   },
   plugins: [require("daisyui")],
 
